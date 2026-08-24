b0eb03145fa7057a69a2c066552116a845503265
sanghavi-lab <58001733+sanghavi-lab@users.noreply.github.com>
2026-08-24 11:35:26 -0500

    Update 02_prep_for_sas_d.py

--- 05_facility_disruption_sample/07_exh2_tab1/01_gather_comorbidity/02_prep_for_sas_d.py
+++ 05_facility_disruption_sample/07_exh2_tab1/01_gather_comorbidity/02_prep_for_sas_d.py
@@ -31,7 +31,7 @@ dask.config.set({
     "distributed.comm.timeouts.tcp": "60s"
 })
 
-client = Client("10.50.87.29:45637")
+client = Client("[redacted]")
 print(client)
 
 # -------------------------
